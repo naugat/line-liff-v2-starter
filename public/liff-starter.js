4e26f62aa056ad0d57519c728bb11f6f5a00b1ee
naugat <74837547+naugat@users.noreply.github.com>
2021-01-09 21:23:16 +0000

    Update liff-starter.js

--- public/liff-starter.js
+++ public/liff-starter.js
@@ -1,5 +1,5 @@
 window.onload = function() {
-    const useNodeJS = false;   // if you are not using a node server, set this value to false
+    const useNodeJS = true;   // if you are not using a node server, set this value to false
     const defaultLiffId = "1655564022-Z9ORX0XK";   // change the default LIFF value if you are not using a node server
 
     // DO NOT CHANGE THIS
