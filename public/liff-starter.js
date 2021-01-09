08a19e1a01522ac909ebb7310234b7d55e5bf61a
naugat <74837547+naugat@users.noreply.github.com>
2021-01-09 21:17:43 +0000

    Update liff-starter.js

--- public/liff-starter.js
+++ public/liff-starter.js
@@ -1,9 +1,9 @@
 window.onload = function() {
-    const useNodeJS = true;   // if you are not using a node server, set this value to false
-    const defaultLiffId = "";   // change the default LIFF value if you are not using a node server
+    const useNodeJS = false;   // if you are not using a node server, set this value to false
+    const defaultLiffId = "1655564022-Z9ORX0XK";   // change the default LIFF value if you are not using a node server
 
     // DO NOT CHANGE THIS
-    let myLiffId = "";
+    let myLiffId = "1655564022-Z9ORX0XK";
 
     // if node is used, fetch the environment variable and pass it to the LIFF method
     // otherwise, pass defaultLiffId
